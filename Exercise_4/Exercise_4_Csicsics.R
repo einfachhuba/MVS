9efd2c7c61a6f03d6070140fe2347ce7a85c2a3a
einfachhuba <huba.csicsics@edu.fh-joanneum.at>
2024-01-03 03:39:23 +0100

    Exercise 4 init

---
+++ Exercise_4/Exercise_4_Csicsics.R
@@ -0,0 +1,19 @@
+#   Exercise 3      #
+#   29.11.2023      #
+#   Huba Csicsics   #
+
+###############################################################################
+## Ames Housing Data
+###############################################################################
+
+# install.packages("modeldata")
+require(modeldata)
+help(modeldata)
+
+# load the data
+data(ames)
+
+#print colnames
+colnames(ames)
+
+help(modeldata)
